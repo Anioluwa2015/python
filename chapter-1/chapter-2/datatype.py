5f1c36890aa2bc6e70272e982bdacbbf054612b5
Anioluwa2015 <Anioluwa2015@gmail.com>
2025-02-06 17:13:14 +0000

    hello

---
+++ chapter-1/chapter-2/datatype.py
@@ -0,0 +1,8 @@
+name=input("enter your name:")
+
+print("\nhello",name,"\n welcome to the world of uk girls")
+
+print(len(name))
+str1="ruby"
+str6="bella"
+print(str1 [1:3])
--- chapter-1/first.py
+++ chapter-1/first.py
@@ -1,1 +1,4 @@
 print("welcome to the uk girl store")
+a=6
+b=600
+print("600+6=",a+b)
