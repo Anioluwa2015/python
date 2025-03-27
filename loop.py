f1d4ac7f0670a06d015e58242aa58bc50d0f7141
Anioluwa2015 <Anioluwa2015@gmail.com>
2025-03-27 17:11:47 +0000

    this is my loop projects!!!!!!!!!!

---
+++ loop.py
@@ -0,0 +1,7 @@
+str=input("enter a word:")
+
+str1=('')
+for i in str:
+    str1 = i + str1
+    print("ORINGINAL WORD =",str)
+    print("BACKWARD=", str1)
---
+++ loopsfor.py
@@ -0,0 +1,3 @@
+name="aunt reema"
+for i in name:
+    print(i)
